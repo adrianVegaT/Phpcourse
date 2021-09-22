e3cc9f4d24e49f0e069565b2a962e54e78f50186
Antonio Vega <adrianvegat@outlook.com>
2021-09-21 23:17:01 -0600

    creando funcion en index.php y helpers.php

---
+++ bloques.php
@@ -0,0 +1,33 @@
+<?php
+
+if (condition){
+    # code..
+}elseif(second condition){
+    # code..
+}else{
+    # code..
+}
+
+switch ($variable) {
+    case 'value':
+        # code...
+        break;
+    
+    default:
+        # code...
+        break;
+}
+
+foreach ($variable as $key => $value) {
+    # code...
+}
+
+$a = 1;
+while ($a <= 10) {
+    # code...
+    a++;
+}
+
+do {
+    # code...
+} while ($a <= 10);
